f82719df0646409a620520e53b1aadd600640537
Emir Yenikale <emirhasankoca@gmail.com>
2026-08-12 19:22:47 +0300

    bring in a pixel typeface for the control panel

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import "@fontsource/press-start-2p";
 import App from "./App.js";
 
 const root = createRoot(document.getElementById("root")!);
