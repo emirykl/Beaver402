715655ca492048e0446b8a5aeb9d5e8844d04101
Emir Yenikale <emirhasankoca@gmail.com>
2026-08-13 15:41:15 +0300

    switch the console to a technical monospace

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import "@fontsource/press-start-2p";
+import "@fontsource/jetbrains-mono/400.css";
+import "@fontsource/jetbrains-mono/500.css";
+import "@fontsource/jetbrains-mono/700.css";
 import App from "./App.js";
 
 const root = createRoot(document.getElementById("root")!);
